c2691ae8eb3c8ed75c5116234683849a17fa4cb0
Léo SENIQUE <leo.senique@gmail.com>
2020-07-01 12:17:14 +0200

    tp 2 correction erreur

--- src/main/java/TestConnexionJdbc.java
+++ src/main/java/TestConnexionJdbc.java
@@ -18,18 +18,9 @@ public class TestConnexionJdbc {
 		String utilisateur = database.getString("database.user");
 		String motDePasse = database.getString("database.pass");
 		Connection connexion = null;
-		Statement statement = null;
 		try {
 			connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
 			System.out.println(connexion);
-			statement = connexion.createStatement();
-			statement.execute("create table ARTICLE (ID int NOT NULL PRIMARY KEY, REF varchar(13) NOT NULL, DESIGNATION\r\n" + 
-					"varchar(255) NOT NULL, PRIX decimal(7,2) NOT NULL, ID_FOU int NOT NULL);\r\n" + 
-					"create table FOURNISSEUR (ID int NOT NULL PRIMARY KEY, NOM varchar(25) NOT NULL);\r\n" + 
-					"create table BON (ID int NOT NULL PRIMARY KEY, NUMERO int, DATE_CMDE DATETIME DEFAULT\r\n" + 
-					"CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, DELAI int, ID_FOU int not null);\r\n" + 
-					"create table COMPO (ID int NOT NULL AUTO_INCREMENT PRIMARY KEY, ID_ART int, ID_BON int, QTE\r\n" + 
-					"int);");
 			
 		} finally {
 			if(connexion != null) {
