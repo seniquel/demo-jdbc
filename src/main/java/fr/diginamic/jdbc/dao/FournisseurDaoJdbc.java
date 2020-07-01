884a900484b146e8fad1310f38aec1a3b271172e
Léo SENIQUE <leo.senique@gmail.com>
2020-07-01 16:15:00 +0200

    Ajout preparedstatements (TP5)

--- src/main/java/fr/diginamic/jdbc/dao/FournisseurDaoJdbc.java
+++ src/main/java/fr/diginamic/jdbc/dao/FournisseurDaoJdbc.java
@@ -15,23 +15,23 @@ import fr.diginamic.jdbc.entites.Fournisseur;
 public class FournisseurDaoJdbc implements FournisseurDao {
 	@Override
 	public List<Fournisseur> extraire() throws SQLException, ClassNotFoundException {
-			//Création de la liste de fournisseurs vide
-			List<Fournisseur> listeFournisseurs = new ArrayList<>(); 
-			try(Connection connexion = connexionCleverCloud()){
-				try(Statement statement = connexion.createStatement(); // Création du statement
+		//Création de la liste de fournisseurs vide
+		List<Fournisseur> listeFournisseurs = new ArrayList<>(); 
+		try(Connection connexion = connexionCleverCloud()){
+			try(Statement statement = connexion.createStatement(); // Création du statement
 					//Exécution de la requête
 					ResultSet resultSet = statement.executeQuery("select * from FOURNISSEUR")){
-					// ajout des résultats dans la liste des fournisseurs
-					while(resultSet.next()) {
-						int id = resultSet.getInt("ID");
-						String nom = resultSet.getString("NOM");
-						listeFournisseurs.add(new Fournisseur(id,nom));
-					}
-				}
-				catch(SQLException e) {
-					System.err.println(e.getMessage());
+				// ajout des résultats dans la liste des fournisseurs
+				while(resultSet.next()) {
+					int id = resultSet.getInt("ID");
+					String nom = resultSet.getString("NOM");
+					listeFournisseurs.add(new Fournisseur(id,nom));
 				}
 			}
+			catch(SQLException e) {
+				System.err.println(e.getMessage());
+			}
+		}
 		return listeFournisseurs;
 	}
 
@@ -40,8 +40,10 @@ public class FournisseurDaoJdbc implements FournisseurDao {
 		try(Connection connexion = connexionCleverCloud()){
 			connexion.setAutoCommit(false);
 			try(Statement statement = connexion.createStatement();  // Création du statement
-				PreparedStatement ps = connexion.prepareStatement("insert into FOURNISSEUR(ID,NOM) values ("+fournisseur.getId()+",'"+fournisseur.getNom()+"')")){
+				PreparedStatement ps = connexion.prepareStatement("insert into FOURNISSEUR(ID,NOM) values (?,?)")){
 				// Exécution de la requête
+				ps.setInt(1, fournisseur.getId());
+				ps.setString(2, fournisseur.getNom());
 				ps.executeUpdate();
 				connexion.commit();
 			}
@@ -58,18 +60,20 @@ public class FournisseurDaoJdbc implements FournisseurDao {
 		try(Connection connexion = connexionCleverCloud()){
 			connexion.setAutoCommit(false);
 			try(Statement statement = connexion.createStatement(); // Création du statement
-				PreparedStatement ps = connexion.prepareStatement("update FOURNISSEUR set NOM='"+nouveauNom+"' where NOM='"+ancienNom+"'")){
-					// Exécution de la requête
-					lignes = ps.executeUpdate();
-					connexion.commit();
-				}
+					PreparedStatement ps = connexion.prepareStatement("update FOURNISSEUR set NOM=? where NOM=?")){
+				// Exécution de la requête
+				ps.setString(1, nouveauNom);
+				ps.setString(2, ancienNom);
+				lignes = ps.executeUpdate();
+				connexion.commit();
+			}
 			catch(SQLException e) {
 				System.err.println(e.getMessage());
 				connexion.rollback();
 			}
 		}
 		return lignes;
-		
+
 	}
 
 	@Override
@@ -78,13 +82,15 @@ public class FournisseurDaoJdbc implements FournisseurDao {
 		try(Connection connexion = connexionCleverCloud()){
 			connexion.setAutoCommit(false);
 			try(Statement statement = connexion.createStatement(); //Création du statement
-				PreparedStatement ps = connexion.prepareStatement("delete from FOURNISSEUR where NOM='"+fournisseur.getNom()+"' and ID="+fournisseur.getId())){
-					// Exécution de la requête
-					int nbLignesSupprimees = ps.executeUpdate();
-					connexion.commit();
-					// Au moins 1 ligne a bien été supprimée
-					supprime = nbLignesSupprimees > 0;
-				}
+					PreparedStatement ps = connexion.prepareStatement("delete from FOURNISSEUR where NOM=? and ID=?")){
+				// Exécution de la requête
+				ps.setString(1, fournisseur.getNom());
+				ps.setInt(2, fournisseur.getId());
+				int nbLignesSupprimees = ps.executeUpdate();
+				connexion.commit();
+				// Au moins 1 ligne a bien été supprimée
+				supprime = nbLignesSupprimees > 0;
+			}
 			catch(SQLException e) {
 				System.err.println(e.getMessage());
 				connexion.rollback();
@@ -92,18 +98,18 @@ public class FournisseurDaoJdbc implements FournisseurDao {
 		}
 		return supprime;
 	}
-	
+
 	private Connection connexionCleverCloud() throws SQLException, ClassNotFoundException {
-	    // Lecture du fichier de propriétés
-	    ResourceBundle database = ResourceBundle.getBundle("clevercloud-database");
-	    // Enregistrement du pilote
+		// Lecture du fichier de propriétés
+		ResourceBundle database = ResourceBundle.getBundle("clevercloud-database");
+		// Enregistrement du pilote
 		Class.forName(database.getString("database.driver"));
 		// Création de la connexion
 		String url = database.getString("database.url");
 		String utilisateur = database.getString("database.user");
 		String motDePasse = database.getString("database.pass");	
 		Connection connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
-	    return connexion;	
+		return connexion;	
 	}
 
 }
--- src/main/java/fr/diginamic/jdbc/dao/TestInsertion.java
+++ src/main/java/fr/diginamic/jdbc/dao/TestInsertion.java
@@ -9,7 +9,7 @@ public class TestInsertion {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
 		FournisseurDaoJdbc daoF = new FournisseurDaoJdbc();
-		daoF.insert(new Fournisseur(4,"L''espace création"));
+		daoF.insert(new Fournisseur(4,"L'espace création"));
 	}
 
 }
